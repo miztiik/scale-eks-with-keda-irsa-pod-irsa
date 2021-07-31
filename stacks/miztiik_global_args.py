c9bb5f643a25e114ff3ad431425fef53c071f2b2
Mystique <13oct08@quantumfoam.uni.cc>
2021-07-31 17:37:09 +0200

    LGTM

--- stacks/miztiik_global_args.py
+++ stacks/miztiik_global_args.py
@@ -6,5 +6,5 @@ class GlobalArgs():
     OWNER = "MystiqueAutomation"
     REPO_NAME = "scale-eks-with-keda-irsa-pod-irsa"
     SOURCE_INFO = f"https://github.com/miztiik/{REPO_NAME}"
-    VERSION = "2020-06-11"
+    VERSION = "2021-06-11"
     MIZTIIK_SUPPORT_EMAIL = ["mystique@example.com", ]
